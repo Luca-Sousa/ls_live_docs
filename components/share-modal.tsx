d769ab8ab1dfca694f957c3969566ed9ab5fa979
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-05-20 09:35:05 -0300

    feat: add permission to update a collaborator's access and remove access

--- components/share-modal.tsx
+++ components/share-modal.tsx
@@ -16,6 +16,7 @@ import Image from "next/image";
 import { Input } from "./ui/input";
 import UserTypeSelector from "./user-type-select";
 import Collaborator from "./collaborator";
+import { updateDocumentAccess } from "@/lib/actions/room.actions";
 
 const ShareModal = ({
   roomId,
@@ -30,7 +31,18 @@ const ShareModal = ({
   const [email, setEmail] = useState("");
   const [userType, setUserType] = useState<UserType>("viewer");
 
-  const shareDocumentHandler = async () => {};
+  const shareDocumentHandler = async () => {
+    setLoading(true);
+
+    await updateDocumentAccess({
+      roomId,
+      email,
+      userType: userType as UserType,
+      updatedBy: user.info,
+    });
+
+    setLoading(false);
+  };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
